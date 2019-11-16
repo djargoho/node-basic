f7952d0b82208ab4b532fbb282a5d9ad36aacfb4
Mordiumaco <mordiumaco@gmail.com>
2019-11-16 23:16:31 +0900

    until query string

--- node-basic/node-inner-module/searchParams.js
+++ node-basic/node-inner-module/searchParams.js
@@ -13,11 +13,14 @@ myURL.searchParams.append('filter', 'es3'); //해당 키를 추가 append(KEY, V
 myURL.searchParams.append('filter', 'es5'); //해당 키를 추가 append(KEY, VALUE);
 console.log(myURL.searchParams.getAll('filter')); 
 
-myURL.searchParams.set('filter', 'es6');
-console.log(myURL.searchParams.getAll('filter'));
+myURL.searchParams.set('filter', 'es6'); //append와 비슷하나 같은 키의 값을 모두 지우고 새로 추가
+console.log(myURL.searchParams.getAll('filter')); 
 
-myURL.searchParams.delete('filter');
-console.log(myURL.searchParams.getAll('filter'));
+myURL.searchParams.delete('filter'); //해당 키를 제거
+console.log(myURL.searchParams.getAll('filter')); 
 
 console.log('searchParams.toString():', myURL.searchParams.toString());
-myURL.search = myURL.searchParams.toString();
+myURL.search = myURL.searchParams.toString(); //조작한 searchParams 객체를 다시 문자열로 만들어 search에 대입하면 주소 객체에 반영됨.
+
+//query를 사용하는 방법도 있지만 query의 경우에는 querystring 모듈을 한 번 더 사용하기 때문에 귀찮다.
+
--- node-basic/node-inner-module/url.js
+++ node-basic/node-inner-module/url.js
@@ -10,7 +10,7 @@ console.log("new URL():", myURL);
 console.log("url.format():", url.format(myURL));
 console.log("------------------------------");
 const parsedUrl = url.parse(
-  "http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor"
+  "http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000&category=nodejs&category=javascript#anchor"
 ); 
 console.log("url.parse():", parsedUrl); //해당 URL 객체를 파싱, WHATWG방식과 다르게 username, password대신 auth 속성, searchParams 대신 query가 있다.
 console.log("url.format():", url.format(parsedUrl)); //WHATWG방식의 url 과 기존 노드의 url 모두 사용가능 분해 되었던 url 객체를 월래상태로 조립
