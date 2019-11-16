aefd1ccfb596dc6c45872f7e7e63dc22686a4c65
DJArgoho <djargoho@gmail.com>
2019-11-16 23:52:00 +0900

    argoho test

--- node-basic/node-inner-module/pbkdf2.js
+++ node-basic/node-inner-module/pbkdf2.js
@@ -1,7 +1,8 @@
 /*
     현재는 주로 pdkdf2나 bcrypt, scrypt라는 알고리즘으로 비밀번호를 암호화 함. 
     node에서는 pbkdf2를 지원.
-    pdkdf2는 간단히 말하면 기존 문자열에 salt라고 불리는 문자열을 붙인 후 해시 알고리즘을 반복해서 적용하는 것     
+    pdkdf2는 간단히 말하면 기존 문자열에 salt라고 불리는 문자열을 붙인 후 해시 알고리즘을 반복해서 적용하는 것
+    꼭 비밀번호는 암호화 하자
 */
 
 const crypto = require("crypto");
